14e1733c68408f27986c4f08766a9a481714355d
Valhallaproject <valhallaproject@outlook.fr>
2021-07-29 22:35:38 +0200

    add routes users/sauces

---
+++ backend/routes/sauces.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const sauceCtrl =  require('../controllers/sauces');
+
+
+router.post('/' ,auth, multer, sauceCtrl.addOneSauce);  
+router.get('/'  ,auth , sauceCtrl.getAllSauce);
+router.get('/:id' ,auth, sauceCtrl.getOneSauce)
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.likeDislike)
+
+
+module.exports = router;
